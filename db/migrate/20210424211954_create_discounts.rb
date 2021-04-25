5e2fdc6a87b43cec0d1b062c27608c2e5a6d3028
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-25 14:58:10 -0600

    Create discounts table and relationships

---
+++ db/migrate/20210424211954_create_discounts.rb
@@ -0,0 +1,10 @@
+class CreateDiscounts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :discounts do |t|
+      t.references :merchant, foreign_key: true
+      t.decimal :percent_off
+      t.integer :min_threshold
+      t.string :status
+    end
+  end
+end
