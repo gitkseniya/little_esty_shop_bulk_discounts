43fd6491b3fc34bea768a49c3f517457cacc5f7f
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-27 00:19:19 -0600

    Add incomplete invoices model spec

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -16,6 +16,25 @@ RSpec.describe InvoiceItem, type: :model do
   end
 
   describe "methods" do
+    it "can return incomplete invoices" do
+      time1 = Time.new(2008,6,21, 13,30,0, "+09:00").utc
+      time2 = Time.new(2006,6,21, 13,30,0, "+09:00").utc
+      time3 = Time.new(2015,6,21, 13,30,0, "+09:00").utc
+
+      invoice1 = create(:invoice, created_at: time1)
+      invoice2 = create(:invoice, created_at: time2)
+      invoice3 = create(:invoice, created_at: time3)
+
+      item1 = create(:item)
+      item2 = create(:item)
+
+      invoice_item1 = create(:invoice_item, invoice: invoice1, item: item1, status: 0)
+      invoice_item2 = create(:invoice_item, invoice: invoice2, item: item1, status: 1)
+      invoice_item3 = create(:invoice_item, invoice: invoice3, item: item2, status: 2)
+
+      expect(InvoiceItem.incomplete_invoices).to eq([invoice2, invoice1])
+    end
+
     it "determines whether a discount has been applied to an invoice item" do
       @merchant1 = create(:merchant)
 
