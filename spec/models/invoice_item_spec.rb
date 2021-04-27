8f1e172acf3912ba34eebe398c4416670f05495b
Kseniya K <kseniya.kudelich@gmail.com>
2021-04-27 00:16:01 -0600

    Add link conditional model test for helper method

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -14,4 +14,29 @@ RSpec.describe InvoiceItem, type: :model do
     it { should have_one(:merchant).through(:item) }
     it { should have_many(:discounts).through(:merchant) }
   end
+
+  describe "methods" do
+    it "determines whether a discount has been applied to an invoice item" do
+      @merchant1 = create(:merchant)
+
+      @cupcake = create(:item, merchant: @merchant1)
+      @cake = create(:item, merchant: @merchant1)
+
+      @discount1 = create(:discount, merchant: @merchant1, percent_off: 10, min_threshold: 5)
+      @discount2 = create(:discount, merchant: @merchant1, percent_off: 15, min_threshold: 10)
+      @discount3 = create(:discount, merchant: @merchant1, percent_off: 20, min_threshold: 15)
+
+      @klaudia = create(:customer)
+      @olivia = create(:customer)
+
+      @invoice1 = create(:invoice, customer: @klaudia)
+      @invoice2 = create(:invoice, customer: @olivia)
+
+      @invoice_item1 = create(:invoice_item, invoice: @invoice1, item: @cupcake, quantity: 11, unit_price: 6)
+      @invoice_item2 = create(:invoice_item, invoice: @invoice2, item: @cake, quantity: 1, unit_price: 28)
+
+      expect(@invoice_item1.link_conditional).not_to eq(nil)
+      expect(@invoice_item2.link_conditional).to eq(nil)
+    end
+  end
 end
